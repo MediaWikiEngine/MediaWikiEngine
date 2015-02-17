fa17acee4edbfa6565d1d74f46142b23edd5b197
Administrator <h.hamstra@gmail.com>
2015-02-17 22:30:30 +0100

    Copyright change.

--- src/com/github/mediawikiengine/actions/exceptions/ActionFailedException.java
+++ src/com/github/mediawikiengine/actions/exceptions/ActionFailedException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright $year - MediaWikiEngine
+ * Copyright 2015 - MediaWikiEngine
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
