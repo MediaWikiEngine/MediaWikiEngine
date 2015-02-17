1754a9db47302b30946c6be95681cd66eabd7f57
Administrator <h.hamstra@gmail.com>
2015-02-17 22:26:58 +0100

    Copyright change. New name of project

--- src/com/github/mediawikiengine/var/NameSpaces.java
+++ src/com/github/mediawikiengine/var/NameSpaces.java
@@ -1,6 +1,5 @@
-/**
- * Copyright 2012 - WebserviceCloud.net
- *
+/*
+ * Copyright $year - MediaWikiEngine
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
@@ -12,8 +11,6 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * <p/>
- * Created on: 24-9-12 23:39
  */
 
 package com.github.mediawikiengine.var;
